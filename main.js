407da353086ef9c146835894fe532e7300458fea
Alex Wells <1walexw@gmail.com>
2013-12-28 17:14:48 -0700

    Remove Eventful from closure

--- main.js
+++ main.js
@@ -1,42 +1,48 @@
-(function(scope){
+
+
+
+
+
+
+
+
+
 		
-	function Eventful(){}
-	
-	Eventful.prototype.addEventListener = function(event,listener){
-		if(!this._eventListeners)this._eventListeners = {};
-		if(!this._eventListeners[event])this._eventListeners[event] = []
-		this._eventListeners[event].push(listener);
+function Eventful(){}
+
+Eventful.prototype.addEventListener = function(event,listener){
+	if(!this._eventListeners)this._eventListeners = {};
+	if(!this._eventListeners[event])this._eventListeners[event] = []
+	this._eventListeners[event].push(listener);
+}
+
+Eventful.prototype.fireEvent = function(event,obj){
+	if(this._eventListeners && this._eventListeners[event]){
+		this._eventListeners[event].forEach(function(listener){
+			listener(obj)
+		});
 	}
-	
-	Eventful.prototype.fireEvent = function(event,obj){
-		if(this._eventListeners && this._eventListeners[event]){
-			this._eventListeners[event].forEach(function(listener){
-				listener(obj)
-			});
-		}
+}
+
+Eventful.prototype.removeListener = function(event,listener){
+	if(this._eventListeners && this._eventListeners[event]){
+		var i = this._eventListeners[event].indexOf(listener);
+		if(i != -1)this.this._eventListeners[event].splice(i,1);
 	}
-	
-	Eventful.prototype.removeListener = function(event,listener){
-		if(this._eventListeners && this._eventListeners[event]){
-			var i = this._eventListeners[event].indexOf(listener);
-			if(i != -1)this.this._eventListeners[event].splice(i,1);
-		}
-	}
-	
-	Eventful.prototype.bubbleEvent = function(obj,event){
-		obj.addEventListener(event,function(eventObj){
-			this.fireEvent(event,eventObj);
-		}.bind(this));
-	}
-	
-	Eventful.prototype.bubbleEvents = function(obj,events){
-		events.forEach(function(event){
-			this.bubbleEvent(obj,event);
-		}.bind(this));
-	}
-	
-	scope.Eventful = Eventful;
-}(this));
+}
+
+Eventful.prototype.bubbleEvent = function(obj,event){
+	obj.addEventListener(event,function(eventObj){
+		this.fireEvent(event,eventObj);
+	}.bind(this));
+}
+
+Eventful.prototype.bubbleEvents = function(obj,events){
+	events.forEach(function(event){
+		this.bubbleEvent(obj,event);
+	}.bind(this));
+}
+
 
 'use strict';
 
